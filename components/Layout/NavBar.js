d27799fee9068895081680f99916dac9f5236972
jesusrq92git <jesusrq92@gmail.com>
2020-08-25 17:05:45 -0500

    Change name class and fill rule

--- components/Layout/NavBar.js
+++ components/Layout/NavBar.js
@@ -63,10 +63,10 @@ const NavBar = props => {
             </Nav>
             <Form className={"center"}>
               <p className={"p-username icon-user"}>
-                <svg className={"icon-user"} width="25px" height="25px" viewBox="0 0 16 16" class="bi bi-person-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <svg className={"icon-user"} width="25px" height="25px" viewBox="0 0 16 16" className="bi bi-person-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                   <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
-                  <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                  <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
+                  <path fillRule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                  <path fillRule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
                 </svg>
               </p>
               <p className={"p-username"}>
